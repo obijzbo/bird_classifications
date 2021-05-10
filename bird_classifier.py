5d486f34cd7652a1017854e4028a302481152a1a
Nahiyan Mubashshir <nahiyanmubashshir@gmail.com>
2021-05-10 20:52:57 +0600

    Initial commit

---
+++ bird_classifier.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  9 19:58:06 2021
+
+@author: tns
+"""
+
+from matplotlib import pyplot as plt
+from keras.datasets import cifar10
+import tflearn
+from tflearn.data_utils import shuffle, to_categorical
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.estimator import regression
+from tflearn.data_preprocessing import ImagePreprocessing
+from tflearn.data_augmentation import ImageAugmentation
+
+# load dataset
+(train_X, train_y), (test_X, test_y) = cifar10.load_data()
+train_X, train_y = shuffle(train_X, train_y)
+
+# summarize loaded dataset
+print('Train: X=%s, y=%s' % (train_X.shape, train_y.shape))
+print('Test: X=%s, y=%s' % (test_X.shape, test_y.shape))
+# plot first few images
+for i in range(9):
+	# define subplot
+	plt.subplot(330 + 1 + i)
+	# plot raw pixel data
+	plt.imshow(train_X[i])
+# show the figure
+plt.show()
+
+# Make sure the data is normalized
+img_prep = ImagePreprocessing()
+img_prep.add_featurewise_zero_center()
+img_prep.add_featurewise_stdnorm()
+
+# Create extra synthetic training data by flipping, rotating and blurring the
+# images on our data set.
+img_aug = ImageAugmentation()
+img_aug.add_random_flip_leftright()
+img_aug.add_random_rotation(max_angle=25.)
+img_aug.add_random_blur(sigma_max=3.)
+
+# Define our network architecture:
+
+# Input is a 32x32 image with 3 color channels (red, green and blue)
+network = input_data(shape=[None, 32, 32, 3],
+                     data_preprocessing=img_prep,
+                     data_augmentation=img_aug)
+
+# Step 1: Convolution
+network = conv_2d(network, 32, 3, activation='relu')
+
+# Step 2: Max pooling
+network = max_pool_2d(network, 2)
+
+# Step 3: Convolution again
+network = conv_2d(network, 64, 3, activation='relu')
+
+# Step 4: Convolution yet again
+network = conv_2d(network, 64, 3, activation='relu')
+
+# Step 5: Max pooling again
+network = max_pool_2d(network, 2)
+
+# Step 6: Fully-connected 512 node neural network
+network = fully_connected(network, 512, activation='relu')
+
+# Step 7: Dropout - throw away some data randomly during training to prevent over-fitting
+network = dropout(network, 0.5)
+
+# Step 8: Fully-connected neural network with two outputs (0=isn't a bird, 1=is a bird) to make the final prediction
+network = fully_connected(network, 2, activation='softmax')
+
+# Tell tflearn how we want to train the network
+network = regression(network, optimizer='adam',
+                     loss='categorical_crossentropy',
+                     learning_rate=0.001)
+
+# Wrap the network in a model object
+model = tflearn.DNN(network, tensorboard_verbose=0, checkpoint_path='bird-classifier.tfl.ckpt')
+
+# Train it! We'll do 100 training passes and monitor it as it goes.
+model.fit(train_X, train_y, n_epoch=25, shuffle=True, validation_set=(test_X, test_y),
+          show_metric=True, batch_size=96,
+          snapshot_epoch=True,
+          run_id='bird-classifier')
+
+# Get the model summary.
+model.summary()
+
+def Res_Plot_Test_Save(name):
+    print("[INFO] Plotting model accuracy and Loss")
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    epochs = range(1, len(acc) + 1)
+    #Train and validation accuracy
+    plt.plot(epochs, acc, 'b', label='Training accurarcy')
+    plt.plot(epochs, val_acc, 'r', label='Validation accurarcy')
+    plt.title('Training and Validation accurarcy')
+    plt.legend()
+
+    plt.figure()
+    #Train and validation loss
+    plt.plot(epochs, loss, 'b', label='Training loss')
+    plt.plot(epochs, val_loss, 'r', label='Validation loss')
+    plt.title('Training and Validation loss')
+    plt.legend()
+    plt.show()
+    
+    print("[INFO] Calculating model accuracy")
+    scores = model.evaluate(x_test, y_test)
+    print(f"Test Accuracy: {scores[1]*100}")
+    
+    print("[INFO] Saving model")
+    model.save(name)
+    
+# Save model when training is complete to a file
+print("Network trained and saved as bird-classifier.tfl!")
